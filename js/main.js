ebd466294178f0a3ca4ddad756684beb62072b6a
Afonso Lima <afonsolima@gmail.com>
2022-07-03 14:05:17 -0300

    - adicionando responsividade ao site - reorganizando estrutura - reorganizando layout

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function openMenu() {
     btnburger.style.display = "none";
-    sidebar.style.display = "flex";
-    sidebar.style.width = "400px";
+    btnclose.style.display = "block";
+    sidebar.style.width = "100%";
 }
 
 function closeMenu() {
     sidebar.style.width = "0px";
-    sidebar.style.display = "none";
-    btnburger.style.display = "flex";
+    // sidebar.style.display = "none";
+    btnburger.style.display = "block";
+    btnclose.style.display = "none";
 }
 
-closeMenu();
+function handleMenuClick() {
+    closeMenu();
+}
+
+
